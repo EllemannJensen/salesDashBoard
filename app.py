5708880722ad4ac27b68e0d961aeffb2928313f7
Elisabeth Waardal <elisabeth@waardal.eu>
2021-01-17 17:05:50 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,179 @@
+ 
+
+#################
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import numpy as np
+from dash.dependencies import Output, Input
+
+data = pd.read_csv("/Users/elisabethwolff/sales_analytics/RandomSalesDataTest.csv", decimal=",",delimiter=";", infer_datetime_format=True)
+# data = data.query("Type == 'Online' and Region == 'A'")
+print(data)
+data["Date"] = pd.to_datetime(data["Date"], errors='coerce', infer_datetime_format=True) #format="%y-%m-%d")
+data.sort_values("Date", inplace=True)
+
+####app = dash.Dash(__name__)
+
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+        "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Ad Analytics!"
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="=^_^=", className="header-emoji"),
+                html.H1(
+                    children="Ad Analytics", className="header-title"
+                ),
+            html.P(
+                children="Analyze the prices of Ads sold"
+                " and the number of sold Ads",
+                className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        ########
+        
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {"label": region, "value": region}
+                                for region in np.sort(data.Region.unique()) #(data.get("Region").unique())
+                            ],
+                            value="A",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Type", className="menu-title"),
+                        dcc.Dropdown(
+                            id="type-filter",
+                            options=[
+                                {"label": ad_type, "value": ad_type}
+                                for ad_type in data.Type.unique() # das und Region wieder zurück ändern? data.get('Type').unique()
+                            ],
+                            value="Online",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range",
+                            className="menu-title"
+                            ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data.Date.min().date(),
+                            max_date_allowed=data.Date.max().date(),
+                            start_date=data.Date.min().date(),
+                            end_date=data.Date.max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        ################
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="volume-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+
+@app.callback(
+    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    [
+        Input("region-filter", "value"),
+        Input("type-filter", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+def update_charts(region, ad_type, start_date, end_date):
+    mask = (
+        (data.Region == region) # test zurück? (data.get("Region") == region)
+        & (data.Type == ad_type) # test (data.get("Type") == ad_type)
+        & (data.Date >= start_date) #ist das richtig? war data.Date >= / compare datetime object to date object? data.Data.dt.date >=? & (data.get("Date") >= start_date)
+        & (data.Date <= end_date) # ? data.Date <=     & (data.get("Date") <= end_date) 
+    )
+    filtered_data = data.loc[mask, :]
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Price"],
+                "Type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Price of Ads",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "€", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Total Volume"],
+                "Type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {"text": "Ads Sold", "x": 0.05, "xanchor": "left"},
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
